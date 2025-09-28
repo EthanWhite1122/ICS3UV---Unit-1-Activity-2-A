af242b56db2add9f62e9d5fc872a63cc43dd233c
Ethan White <ethan.white1@stu.ocsb.ca>
2025-09-28 16:08:28 +0000

    Added proper formatting like done message and documentation header comment

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Ethan White
+ * @version 1.0.0
+ * @date 2025-09-28
+ * @fileoverview This is the traditional "Hello, World!" program.
+ */
+
+// print out the gretting
+console.log("Hello, World!");
+
+console.log("\nDone")
